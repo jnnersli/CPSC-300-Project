71199a72f5250611f68f8d40a275fa4011dde48f
JoshuaMuir <36777693+JoshuaMuir@users.noreply.github.com>
2019-10-29 03:20:30 -0700

    Delete randscript.js

--- html/randscript.js
+++
@@ -1,7 +0,0 @@
-function toggleText(button_id) {
-	if(document.getElementById('button_id').textContent == "Lock") {
-		document.getElementById('button_id').textContent = "Unlock";
-	} else {
-		document.getElementById('button_id').text="Lock"
-	}
-}
